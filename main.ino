8e72b2a93b2b1edf284ab2eb4ce59713d2916324
jvltecno <83773735+jvltecno@users.noreply.github.com>
2021-05-14 18:40:57 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -10,12 +10,19 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);  // Inicia el LCD en la dirección 0x27, con
 //-------------------------------Char Specified-------------------------------------------------------------------------------
 
 //-------------------------------Variables-------------------------------------------------------------------------------------
-float mash[];
+float mash[4][4];
 float temp;
-float tempWashing;
-float tempLevature;
-byte timeHervor;
+float tempWash;
+float tempTrans;
+byte timeBoil;
 byte timeAddit [5];
+float tempBoil=100;
+
+int timeInit=0;
+int timeCurrent=millis()/1000;
+int timeMeasured=0;
+byte xTime=0;
+byte xTemp=0;
 
 
 //app
@@ -43,20 +50,34 @@ void set(){
     lcd.print(" a Droid-veza   ");
 
 }
+/--------------------------------------------
 void mash(){
-    if(timeMeter-)
+    
+xTime=mash[][];
+xTemp=mash[][];
+
+if(timeMeasured<xTime){
+    If(timeConcurrent-timeInit<1){ /mide tiempo trnascurrido del proceso
+       timeMeasured++;
+       timeInit=timeConcurrent;
+
+       Lcd.cursor(0,0); // muestra datos
+       Lcd.print("Time=");
+       Lcd.print(timeMessured);
+       Lcd.cursor(0,9);
+       Lcd.print("TimO=");
+       Lcd.print(xTime);
+       Lcd.cursor(1,0);
+       Lcd.print("Temp=");
+       Lcd.print(temp);
+       Lcd.cursor(0,9);
+       Lcd.print("TemO=");
+       Lcd.print(xTemp);
+
+}
 }
 
+/----------------------------------------------
 void boil (){
-
-}
-
-
-
-//-------------------------------lcd--------------------------------------------------------------------------------------------
-
-//--------------------------------Termocupla-------------------------------------------------------------------------------------
-void tempConcurrent(){
-    temp=;//revisar termocupla
 }
 
